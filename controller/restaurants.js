feaaf7b2b8c0a0f49ba938c30d48408b515068fd
tayeb <tayebazersouissi@gmail.com>
2019-09-01 21:40:57 +1300

    getbyid put delete restaurants

--- controller/restaurants.js
+++ controller/restaurants.js
@@ -46,3 +46,109 @@ exports.AddRestaurant = function (req, res) {
         }
     });
 }
+
+
+exports.getRestaurantById = function (req, res) {
+    Restaurant.findById(req.params.id, (err, data) => {
+        if (err) {
+            res.status(500).json({
+                success: false,
+                error: err,
+                message: 'cant access to dtabase'
+            })
+        } else {
+            if (data) {
+                res.status(202).json({
+                    success: true,
+                    restaurants: data
+                })
+            } else {
+                res.status(404).json({
+                    success: false,
+                    message: 'not found',
+                    restaurants: data
+                })
+            }
+        }
+    })
+}
+
+
+exports.updateRestaurant = function (req, res) {
+    Restaurant.findByIdAndUpdate(req.params.id, req.body, function (err) {
+        if (err) {
+
+            res.status(500).json({
+                success: false,
+                message: 'cannot update',
+                error: err
+
+            })
+
+        } else {
+            Restaurant.findById(req.params.id, (err, data) => {
+                if (err) {
+                    res.status(500).json({
+                        success: false,
+                        error: err,
+                        message: 'cant access to dtabase'
+                    })
+                } else {
+                    if (data) {
+                        res.status(202).json({
+                            success: true,
+                            restaurants: data
+                        })
+                    } else {
+                        res.status(404).json({
+                            success: false,
+                            message: 'not found',
+                            restaurants: data
+                        })
+                    }
+                }
+            })
+        }
+
+    })
+}
+
+exports.deleteRestaurant = function (req, res) {
+    Restaurant.findById(req.params.id, (err, data) => {
+        if (err) {
+            res.status(500).json({
+                success: false,
+                error: err,
+                message: 'cant access to dtabase'
+            })
+        } else {
+            if (data) {
+                //remove
+                Restaurant.findById(req.params.id).remove((err, dtat) => {
+                    if (err) {
+
+                        res.status(500).json({
+                            success: false,
+                            error: err,
+                            message: 'connot delete plat'
+                        })
+
+                    } else {
+
+                        res.status(200).json({
+                            success: true,
+                            message: 'plat delete'
+                        })
+
+                    }
+                })
+            } else {
+                res.status(404).json({
+                    success: false,
+                    message: 'plat for delete not found',
+                    restaurants: data
+                })
+            }
+        }
+    })
+}
--- routes/restaurants.js
+++ routes/restaurants.js
@@ -7,4 +7,10 @@ router.get('/get', restController.getAllRestaurant);
 
 router.post('/add', restController.AddRestaurant);
 
+router.get('/get/:id',restController.getRestaurantById);
+
+router.put('/update/:id',restController.updateRestaurant);
+
+router.delete('/delete/:id',restController.deleteRestaurant);
+
 module.exports = router;
